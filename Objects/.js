39cd5277b21909e2810f83f443b220877efb4d09
muskanmandal <mandalmuskan12gmail.com>
2024-04-25 19:56:13 +0530

    objects and objects literals

---
+++ Objects/.js
@@ -0,0 +1,18 @@
+// objects and objects literals
+
+let person = {
+  name: "muskan",
+  age: 22,
+  place: "delhi",
+  friends: ["preeti", "harshita"],
+};
+
+// create an object literal for the properties of thread/twitter post which includes - username, content, likes, reposts, tags
+
+const thread = {
+  username: "@muskanmandal",
+  content: "This is my first post",
+  likes: 100,
+  reposts: 10,
+  tags: ["@harshita", "@preeti"],
+};
